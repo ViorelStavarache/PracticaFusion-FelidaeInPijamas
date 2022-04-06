3f1d6f0aaeeef2087ddfc9d0bc2b93f8dfdc5c83
baffy <baffy@correo.ro>
2022-04-06 20:39:26 +0200

    Rama-Viorel

---
+++ src/praticaFusion/Gato.java
@@ -0,0 +1,51 @@
+package praticaFusion;
+
+public class Gato extends Mascota{
+
+	// Atr
+	
+	private boolean likeWater;
+	private int eye;
+	private String color;
+
+	// Con
+	
+	public Gato(String nombre, boolean water, int eye, String color) {
+		super(nombre);
+		this.likeWater = water;
+		this.eye = eye;
+		this.color = color;
+	}
+
+	// S&G
+	
+	public boolean isLikeWater() {
+		return likeWater;
+	}
+
+	public void setLikeWater(boolean likeWater) {
+		this.likeWater = likeWater;
+	}
+
+	public int getEye() {
+		return eye;
+	}
+
+	public void setEye(int eye) {
+		this.eye = eye;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public void send() {
+		System.out.println("Soy " + this.getColor() + " y el agua es " + this.isLikeWater() + " y tengo " + 
+				this.eye + " ojos");
+	}
+	
+}
--- src/praticaFusion/Principal.java
+++ src/praticaFusion/Principal.java
@@ -4,7 +4,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Gato animal = new Gato("Miriam", false, 2, "Blanca");
+		animal.MandarMensaje();
 	}
 
 }
