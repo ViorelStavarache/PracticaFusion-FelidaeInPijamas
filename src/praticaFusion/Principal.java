469abd625a09d6aceff7d4378feb3dddbfffa2fe
Luisito <luisillo@correo.com>
2022-04-06 20:26:15 +0200

    rama-Luis_Javier_Sanz

--- src/praticaFusion/Principal.java
+++ src/praticaFusion/Principal.java
@@ -4,7 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PinguinoNomadico pinguino = new PinguinoNomadico("Manuel","verde");
+		pinguino.mandarMensaje();
 	}
 
 }
